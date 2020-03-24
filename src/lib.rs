eb6f744e483abc538b329dff6141a7266495987d
gihrig <glen@arkadias.com>
2020-03-24 00:12:10 -0700

    Add tailwind styled buttons in Seed macros
    - Increment and Decrement buttons are functional

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use seed::{*, prelude::*};
-
+use seed::{prelude::*, *};
 
 struct Model {
     pub val: i32,
@@ -7,34 +6,43 @@ struct Model {
 
 impl Default for Model {
     fn default() -> Self {
-        Self {
-            val: 0,
-        }
+        Self { val: 0 }
     }
 }
-
 
 #[derive(Clone)]
 enum Msg {
     Increment,
+    Decrement,
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::Increment => model.val += 1,
+        Msg::Decrement => model.val -= 1,
     }
 }
 
-
 fn view(model: &Model) -> impl View<Msg> {
-    button![
-        simple_ev(Ev::Click, Msg::Increment),
-        format!("Hello, World × {}", model.val)
+    nodes![
+        button![
+            class!("ml-4 mt-4 bg-green-500 text-white p-2 rounded text-2xl font-bold"),
+            simple_ev(Ev::Click, Msg::Increment),
+            format!("+")
+        ],
+        button![
+            class!("ml-4 mt-4 bg-red-500 text-white p-2 rounded text-2xl font-bold"),
+            simple_ev(Ev::Click, Msg::Decrement),
+            format!("-")
+        ],
+        span![
+            class!("ml-4 mt-4 bg-blue-500 text-white p-2 rounded text-2xl font-bold"),
+            format!("{}", model.val)
+        ],
     ]
 }
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    App::builder(update, view)
-        .build_and_start();
+    App::builder(update, view).build_and_start();
 }
