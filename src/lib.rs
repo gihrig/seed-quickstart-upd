b21e22c1a75e4b4cb1cac9845c61799cecb96a03
gihrig <glen@arkadias.com>
2020-03-29 21:18:40 -0700

    demonstrate use of static html

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 use seed::{prelude::*, *};
 
+// mod Home {
+//     fn view() -> Vec<Node<Msg>> {
+//         raw!(
+//             <button class="ml-4 mt-4 bg-green-500 text-white p-2 rounded text-2xl font-bold">+</button>
+//             <button class="ml-4 mt-4 bg-red-500 text-white p-2 rounded text-2xl font-bold">-</button>
+//             <span class="ml-4 mt-4 bg-blue-500 text-white p-2 rounded text-2xl font-bold">0</span>
+//         )
+//     }
+// }
+
 struct Model {
     pub val: i32,
 }
@@ -24,22 +34,28 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 }
 
 fn view(model: &Model) -> impl View<Msg> {
-    nodes![
-        button![
-            class!("ml-4 mt-4 bg-green-500 text-white p-2 rounded text-2xl font-bold"),
-            simple_ev(Ev::Click, Msg::Increment),
-            format!("+")
-        ],
-        button![
-            class!("ml-4 mt-4 bg-red-500 text-white p-2 rounded text-2xl font-bold"),
-            simple_ev(Ev::Click, Msg::Decrement),
-            format!("-")
-        ],
-        span![
-            class!("ml-4 mt-4 bg-blue-500 text-white p-2 rounded text-2xl font-bold"),
-            format!("{}", model.val)
-        ],
-    ]
+    let html_str = r###"
+<button class="ml-4 mt-4 bg-green-500 text-white p-2 rounded text-2xl font-bold">+</button>
+<button class="ml-4 mt-4 bg-red-500 text-white p-2 rounded text-2xl font-bold">-</button>
+<span class="ml-4 mt-4 bg-blue-500 text-white p-2 rounded text-2xl font-bold">0</span>
+"###;
+    raw!(html_str)
+    // nodes![
+    //     button![
+    //         class!("ml-4 mt-4 bg-green-500 text-white p-2 rounded text-2xl font-bold"),
+    //         simple_ev(Ev::Click, Msg::Increment),
+    //         format!("+")
+    //     ],
+    //     button![
+    //         class!("ml-4 mt-4 bg-red-500 text-white p-2 rounded text-2xl font-bold"),
+    //         simple_ev(Ev::Click, Msg::Decrement),
+    //         format!("-")
+    //     ],
+    //     span![
+    //         class!("ml-4 mt-4 bg-blue-500 text-white p-2 rounded text-2xl font-bold"),
+    //         format!("{}", model.val)
+    //     ],
+    // ]
 }
 
 #[wasm_bindgen(start)]
